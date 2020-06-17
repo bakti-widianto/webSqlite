92a44c4621323b9a9d9de41b030c068506ae1ba1
bakti <bakti.widianto@gmail.com>
2020-06-17 16:26:32 +0700

    add data to database

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const sqlite3 = require('sqlite3').verbose();
 const port = 3000
 
 const db_name = path.join(__dirname, "bread.db")
-const db = new sqlite3.Database(db_name, err => {
+const db = new sqlite3.Database(db_name, (err) => {
     if (err) {
         return console.error(err.message)
     }
@@ -44,6 +44,20 @@ app.get('/', (req, res) => {
 
 app.get('/add', (req, res) => {
     res.render('add');
+})
+
+app.post('/add', (req, res) =>{
+    let hasil = req.body;
+    db.serialize(() => {
+        let sql = ('INSERT INTO bread (string, integer, float, date, boolean) VALUES(?,?,?,?,?)')
+        db.run(sql, [hasil.string, parseInt(hasil.integer), parseFloat(hasil.float), hasil.date, JSON.parse(hasil.boolean)], (err)=>{
+            if (err){
+                throw err;
+            }
+            res.redirect('/')
+        })
+        
+    })
 })
 
 
